f7b32c97a793890a4d3688df205efaffbb6ff448
DijaAli <120082436+dijaehyaa@users.noreply.github.com>
2022-12-08 12:14:47 +0500

    Add files via upload

---
+++ Main.kt
@@ -0,0 +1,10 @@
+fun main(args: Array<String>) {
+ //  var greeting: string ="hello word"
+   // var greeting = "Hello World!"
+  //  println("$greeting")
+   // var greeting = "Hello World!"
+  //  greeting = "Hello Kotlin!"
+    val greeting = "Hello World!"
+    //greeting = "Hello Kotlin!"
+    println("$greeting")
+}
